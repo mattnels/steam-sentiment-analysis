3927a50f5b90b33fb1b468bcc9e9533c75bfab03
mn41 <mattnelson41dev@gmail.com>
2019-04-30 21:56:06 -0400

    moving to public repo

---
+++ back-end/server.py
@@ -0,0 +1,96 @@
+#
+# Server for REST endpoints
+#
+# run as: python server.py
+#
+# curl -X POST http://localhost:5000/recommend -H 'Content-Type: application/json' -d '{"review":"I like video game","game":"dota"}'
+#
+
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Resource, Api, reqparse, abort
+
+from Recommendation import get_recommendation
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('review')
+parser.add_argument('score')
+parser.add_argument('game')
+
+#
+# Map each game here
+#
+game_map = (
+    'batmanarkhamknight',
+    'batmanarkhamknightsplit',
+    'dota2',
+    'dota2split',
+    'gtaV',
+    'gtaVsplit',
+    'nomanssky',
+    'nomansskysplit',
+    'payday2',
+    'payday2split',
+    'totalfull',
+    'totalsplit'
+)
+
+
+class RecommendResource(Resource):
+    """   
+    API Resource handler for recommend API
+    """
+    def _func(self, review, score, game):
+        """
+        Drive calling the scoring engine
+        """
+        try:
+            reviews_txt_file = '{0}reviews.txt'.format(game)
+            scores_txt_file = '{0}scores.txt'.format(game)
+
+            isRecommended = get_recommendation(reviews_txt_file, scores_txt_file, review, score, game)
+            return {
+                'review': review,
+                'game': game,
+                'recommended': isRecommended
+            }
+        except Exception as ex:
+            print(ex)
+            self._abort('Exception: {0}'.format(ex), 400)
+    
+    def _abort(self, msg, status):
+        """
+        Abort on error
+        """
+        abort(status, message=msg)
+
+    def post(self):
+        """
+        Handle HTTP POST
+        """
+
+        #
+        # Validate payload
+        #
+        args = parser.parse_args()
+        if args['review'] is None:
+            self._abort('Must supply "review" in post data', 404)
+        elif args['score'] is None:
+            self._abort('Must supply "game" in post data', 404)
+        elif args['game'] is None:
+            self._abort('Must supply "game" in post data', 404)
+        elif args['game'] not in game_map:
+            self._abort('Game "{0}" not in game map'.format(args['game']), 404)
+
+        result = self._func(args['review'], args['score'], args['game'])
+        return result, 200
+
+api.add_resource(RecommendResource, '/recommend')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    LayoutModule,
+    OverlayModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
